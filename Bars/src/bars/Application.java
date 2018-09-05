936df457407489baf1ba77b122a2f0f843b49c4d
Jean Pierre Araya Meléndez <jpierream898@gmail.com>
2018-09-04 21:04:34 -0600

    Agregué un comentario y eliminé comentarios por defecto

--- Bars/src/bars/Application.java
+++ Bars/src/bars/Application.java
@@ -1,24 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package bars;
 
 import bars.controller.Controller;
 import bars.model.Model;
 import bars.view.View;
 
-/**
- *
- * @author Escinf
- */
 public class Application {
     public static void main(String[] args) {
         Model model=new Model() ;
         View view = new View();
         Controller controller = new Controller(model,view);
         view.setVisible(true);
-
+        // Fin del main
     }
 }
